4eeef9a73b30d904c70a712f8ca495b30d6ba6cf
linxiaohui <1025111254@qq.com>
2021-05-17 09:49:30 +0800

    fix 回滚找bug，发现react-dom包的名称不知何时，被去掉了-变成reactdom，报错一直说找不到reactdom

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // main.js
 import React from "react";
-import { render } from "reactdom";
+import { render } from "react-dom";
 import TestCon from "./test";
 import "./main.css";
-
 render(<TestCon />, document.getElementById("root"));
